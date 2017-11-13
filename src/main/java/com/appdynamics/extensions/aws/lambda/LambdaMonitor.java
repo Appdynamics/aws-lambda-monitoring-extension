52fdd17c80cc093422b5e61fb3015ea05d7eda17
Aditya Jagtiani <aditya.jagtiani@ajagtiani-mac.local>
2017-11-13 11:45:51 -0800

    Changes good but the branch name is incorrect

--- src/main/java/com/appdynamics/extensions/aws/lambda/LambdaMonitor.java
+++ src/main/java/com/appdynamics/extensions/aws/lambda/LambdaMonitor.java
@@ -5,6 +5,7 @@ import static com.appdynamics.extensions.aws.Constants.METRIC_PATH_SEPARATOR;
 import com.appdynamics.extensions.aws.SingleNamespaceCloudwatchMonitor;
 import com.appdynamics.extensions.aws.collectors.NamespaceMetricStatisticsCollector;
 import com.appdynamics.extensions.aws.config.Configuration;
+import com.appdynamics.extensions.aws.lambda.config.LambdaConfiguration;
 import com.appdynamics.extensions.aws.metric.processors.MetricsProcessor;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
@@ -12,22 +13,22 @@ import org.apache.log4j.Logger;
 /**
  * @author Satish Muddam
  */
-public class LambdaMonitor extends SingleNamespaceCloudwatchMonitor<Configuration> {
+public class LambdaMonitor extends SingleNamespaceCloudwatchMonitor<LambdaConfiguration> {
 
     private static final Logger LOGGER = Logger.getLogger("com.singularity.extensions.aws.LambdaMonitor");
 
     private static final String DEFAULT_METRIC_PREFIX = String.format("%s%s%s%s",
             "Custom Metrics", METRIC_PATH_SEPARATOR, "Amazon Lambda", METRIC_PATH_SEPARATOR);
 
-    public LambdaMonitor() {
-        super(Configuration.class);
+    LambdaMonitor() {
+        super(LambdaConfiguration.class);
         LOGGER.info(String.format("Using AWS Lambda Monitor Version [%s]",
                 this.getClass().getPackage().getImplementationTitle()));
     }
 
     @Override
     protected NamespaceMetricStatisticsCollector getNamespaceMetricsCollector(
-            Configuration config) {
+            LambdaConfiguration config) {
         MetricsProcessor metricsProcessor = createMetricsProcessor(config);
 
         return new NamespaceMetricStatisticsCollector
@@ -46,14 +47,15 @@ public class LambdaMonitor extends SingleNamespaceCloudwatchMonitor<Configuratio
     }
 
     @Override
-    protected String getMetricPrefix(Configuration config) {
+    protected String getMetricPrefix(LambdaConfiguration config) {
         return StringUtils.isNotBlank(config.getMetricPrefix()) ?
                 config.getMetricPrefix() : DEFAULT_METRIC_PREFIX;
     }
 
-    private MetricsProcessor createMetricsProcessor(Configuration config) {
+    private MetricsProcessor createMetricsProcessor(LambdaConfiguration config) {
         return new LambdaMetricsProcessor(
                 config.getMetricsConfig().getMetricTypes(),
-                config.getMetricsConfig().getExcludeMetrics());
+                config.getMetricsConfig().getExcludeMetrics(),
+                config.getIncludeLambdaFunctions());
     }
 }
